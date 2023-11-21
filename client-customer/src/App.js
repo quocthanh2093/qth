d7245b089e521487a5092f64efc8e9a4ff3f553a
quocthanh2093 <107842420+quocthanh2093@users.noreply.github.com>
2023-11-21 14:21:54 +0700

    Add files via upload

---
+++ client-customer/src/App.js
@@ -0,0 +1,20 @@
+//CLI: npm install axios --save
+import axios from 'axios';
+import './App.css';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import MyProvider from './contexts/MyProvider';
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <BrowserRouter >
+          <Main />
+        </BrowserRouter>
+      </MyProvider>
+    );
+  }
+}
+export default App;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.ai5y6na.mongodb.net',
+  DB_USER: 'qthanh3',
+  DB_PASS: 'qthanh3',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'VoQuocThanh',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: 'voquocthanh200903@gmail.com', // gmail service
+  EMAIL_PASS: 'nuuivpvrxxllkcdu'
+};
+module.exports = MyConstants;
